46f366f4aad8f7c3c520f8ed75b7b8aa06640b01
chicagollama <mariia.lomovskaia@gmail.com>
2024-04-14 19:15:08 +0300

    Add runner.sh to run all pipeline one-click

---
+++ drug_modalities_distribution/runner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm results/*
+
+python3 discover_drug.py
+python3 discover_moa.py
+python3 discover_target.py
+python3 merge.py
+python3 merged2plot.py
+
+cd results ; for ilog in drug*.log moa*.log target*.log merge*.log ; do cat ${ilog} >> dmd.log  ; done ; cd ..
